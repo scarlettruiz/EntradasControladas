b63909a4f063683633a9f40ab42b6fdd437aa4b6
scarlettruiz <scarlettruizlopez50@gmail.com>
2024-05-07 15:19:28 -0700

    cambios

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,33 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React, { useState } from 'react'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+
+function App() {
+      const [miTexto,setMiTexto] = useState('')
+      const handleText = (event) => {
+      setMiTexto(event.target.value)
+    
+        miTexto(result)
+    
+      }
+
+    
+
+   
+
+    return (
+        <div>
+          <div>
+            Capture su texto
+          </div>
+          <input type="text" onChange={handleText} onSubmit={miTexto}/ >
+          <h3>Su Texto</h3>
+          <p>
+            {miTexto}
+          </p>
+        </div>
+    );
 }
 
-export default App
+export default App;
